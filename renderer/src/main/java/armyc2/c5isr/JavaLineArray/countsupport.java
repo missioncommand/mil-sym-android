a5a9e5af327f38fdb4293f5b878839e6fe5357a6
John Perret <john.p.perret.civ@army.mil>
2025-06-03 11:31:23 -0400

    TacticalLines.EXPLOIT

--- renderer/src/main/java/armyc2/c5isr/JavaLineArray/countsupport.java
+++ renderer/src/main/java/armyc2/c5isr/JavaLineArray/countsupport.java
@@ -166,6 +166,7 @@ public final class countsupport
                     break;
                 case TacticalLines.ROADBLK:
                 case TacticalLines.FERRY:
+                case TacticalLines.EXPLOIT:
                     count=8;
                     break;
                 case TacticalLines.NAVIGATION:
