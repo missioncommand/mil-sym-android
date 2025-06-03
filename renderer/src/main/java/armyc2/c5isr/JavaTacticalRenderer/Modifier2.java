2da1e97ba66114c0c58325694852f96be06c6a83
John Perret <john.p.perret.civ@army.mil>
2025-06-03 14:42:17 -0400

    TacticalLines.INFILTRATION

--- renderer/src/main/java/armyc2/c5isr/JavaTacticalRenderer/Modifier2.java
+++ renderer/src/main/java/armyc2/c5isr/JavaTacticalRenderer/Modifier2.java
@@ -596,6 +596,9 @@ public class Modifier2 {
                 case TacticalLines.TRIP:
                     label = "t";
                     break;
+                case TacticalLines.INFILTRATION:
+                    label = "IN";
+                    break;
                 case TacticalLines.FRONTAL_ATTACK:
                     label = "A";
                     break;
@@ -3598,6 +3601,7 @@ public class Modifier2 {
                 case TacticalLines.MFLANE:
                 case TacticalLines.ENVELOPMENT:
                 case TacticalLines.MOBILE_DEFENSE:
+                case TacticalLines.INFILTRATION:
                     break;
                 default:
                     return;
@@ -4019,6 +4023,9 @@ public class Modifier2 {
                     break;
                 case TacticalLines.MOBILE_DEFENSE:
                     AddIntegralModifier(tg, label, area, 0, 16, 16, true);
+                    break;
+                case TacticalLines.INFILTRATION:
+                    AddIntegralModifier(tg, label, aboveMiddle, 0, 0, 1, true);
                     break;
                 default:
                     break;
