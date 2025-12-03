0514eccb8f4ad35192b23888836cd9dea97c6ac9
C5ISR\Michael.P.Spinelli <michael.p.spinelli6.civ@army.mil>
2025-12-03 14:57:54 -0500

    doc update for mapScale

--- renderer/src/main/java/armyc2/c5isr/web/render/WebRenderer.java
+++ renderer/src/main/java/armyc2/c5isr/web/render/WebRenderer.java
@@ -248,6 +248,7 @@ public final class WebRenderer /* extends Applet */ {
      * @param scale A number corresponding to how many meters one meter of our 
      * map represents. A value "50000" would mean 1:50K which means for every 
      * meter of our map it represents 50000 meters of real world distance.
+     * <a href="https://github.com/missioncommand/mil-sym-java/wiki/2525D--Renderer-Overview#3316-scale-for-3d">Scale Calculation Example</a>
      * @param bbox The viewable area of the map.  Passed in the format of a
      * string "lowerLeftX,lowerLeftY,upperRightX,upperRightY." Not required
      * but can speed up rendering in some cases.
@@ -383,6 +384,7 @@ public final class WebRenderer /* extends Applet */ {
      * @param scale         A number corresponding to how many meters one meter of our
      *                      map represents. A value "50000" would mean 1:50K which means for every
      *                      meter of our map it represents 50000 meters of real world distance.
+     *                      <a href="https://github.com/missioncommand/mil-sym-java/wiki/2525D--Renderer-Overview#3316-scale-for-3d">Scale Calculation Example</a>
      * @param bbox          The viewable area of the map.  Passed in the format of a
      *                      string "lowerLeftX,lowerLeftY,upperRightX,upperRightY." Not required
      *                      but can speed up rendering in some cases.
@@ -461,6 +463,7 @@ public final class WebRenderer /* extends Applet */ {
 	 *            represents. A value "50000" would mean 1:50K which means for
 	 *            every meter of our map it represents 50000 meters of real
 	 *            world distance.
+     *            <a href="https://github.com/missioncommand/mil-sym-java/wiki/2525D--Renderer-Overview#3316-scale-for-3d">Scale Calculation Example</a>
 	 * @param bbox
 	 *            The viewable area of the map. Passed in the format of a string
 	 *            "lowerLeftX,lowerLeftY,upperRightX,upperRightY." Not required
@@ -543,6 +546,7 @@ public final class WebRenderer /* extends Applet */ {
      *            represents. A value "50000" would mean 1:50K which means for
      *            every meter of our map it represents 50000 meters of real
      *            world distance.
+     *            <a href="https://github.com/missioncommand/mil-sym-java/wiki/2525D--Renderer-Overview#3316-scale-for-3d">Scale Calculation Example</a>
      * @param bbox
      *            The viewable area of the map. Passed in the format of a string
      *            "lowerLeftX,lowerLeftY,upperRightX,upperRightY." Not required
@@ -593,6 +597,7 @@ public final class WebRenderer /* extends Applet */ {
      * @param scale A number corresponding to how many meters one meter of our
      * map represents. A value "50000" would mean 1:50K which means for every
      * meter of our map it represents 50000 meters of real world distance.
+     * <a href="https://github.com/missioncommand/mil-sym-java/wiki/2525D--Renderer-Overview#3316-scale-for-3d">Scale Calculation Example</a>
      * @param bbox The viewable area of the map.  Passed in the format of a
      * string "lowerLeftX,lowerLeftY,upperRightX,upperRightY." Not required
      * but can speed up rendering in some cases.
@@ -640,6 +645,7 @@ public final class WebRenderer /* extends Applet */ {
      * @param scale A number corresponding to how many meters one meter of our
      * map represents. A value "50000" would mean 1:50K which means for every
      * meter of our map it represents 50000 meters of real world distance.
+     * <a href="https://github.com/missioncommand/mil-sym-java/wiki/2525D--Renderer-Overview#3316-scale-for-3d">Scale Calculation Example</a>
      * @param bbox The viewable area of the map.  Passed in the format of a
      * string "lowerLeftX,lowerLeftY,upperRightX,upperRightY." Not required
      * but can speed up rendering in some cases.
